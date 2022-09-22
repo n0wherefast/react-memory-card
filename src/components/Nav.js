3e0c33ff23715f85f1df676bf1ff8d7b732811a0
Alessio <16mmstudios@gmail.com>
2022-09-22 19:12:47 +0200

    initialized bootstrap
    created components and defined navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = () => {
+   return(
+    <nav className="navbar navbar-dark bg-dark">
+  <div className="container-fluid">
+    <span className="navbar-brand mb-0 h1">Star Wars Memory Game</span>
+  </div>
+</nav>
+   )
+}
+export default Nav
