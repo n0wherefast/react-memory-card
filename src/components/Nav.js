913332d0bf2953f11501903e2694d7af7d3942c4
Alessio <16mmstudios@gmail.com>
2022-09-30 17:34:01 +0200

    fixed css for mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = (props) => {
           <nav className="navbar navbar-dark bg-dark ">
                <div className="container-fluid">
                     <span className="navbar-brand mb-0 h1" style={{ color:"yellow",alignItems:'center'}}>
-                          <h1>Star Wars Memory Game</h1>
+                          <h2>Star Wars Memory Game</h2>
                           <div style={{display:'flex' ,alignItems:'center'}}>Score: {props.score} <h4 style={{ display:`${props.win}` ,marginLeft:'1rem'}}>WIN</h4></div>
                           <div>Best Score: {props.bestScore}</div>
                           
